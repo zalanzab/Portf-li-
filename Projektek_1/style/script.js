f2436e8b1998b395bea58517275bfc330ae962f6
Zab Zalan <zalan.zab@gmail.com>
2026-04-16 18:02:22 +0200

    40

--- Projektek_1/style/script.js
+++ Projektek_1/style/script.js
@@ -1,4 +1,4 @@
-
+﻿
 function scrollToSection(id) {
 
     document.getElementById(id).scrollIntoView({
--- Projektek_3/style/script.js
+++ Projektek_3/style/script.js
@@ -1,4 +1,4 @@
-function scrollToSection(id) {
+﻿function scrollToSection(id) {
 
     document.getElementById(id).scrollIntoView({
         behavior: "smooth"
